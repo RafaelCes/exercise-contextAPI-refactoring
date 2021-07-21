ee8619aac41f185e96ae1d05f4302164b8d4baea
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-07-21 16:15:41 -0300

    exercicio 1 substitui redux por context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
+import carContext from './CarContext';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    };
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(color) {
+    this.setState((previousState) => ({
+      [color]: !previousState[color],
+    }));
+  }
+
+  render(){
+    const carObj = { 
+      red: this.state.red,
+      blue: this.state.blue,
+      yellow: this.state.yellow,
+      moveCar: this.moveCar,
+    }
+    
+    return (
+      <carContext.Provider value={carObj}>
+        <Cars />
+      </carContext.Provider>
+      );
+    }
+  }
 
 export default App;
